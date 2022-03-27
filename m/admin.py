96689a411afee61523b26c18ce9f3c2ab67152ab
Horma-mohamed <horma495@gmail.com>
2022-03-27 08:20:32 +0200

    change models

--- m/admin.py
+++ m/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from .models import * 
 # Register your models here.
-from django_markdown.admin import MarkdownModelAdmin
 class InlineImages(admin.StackedInline):
     model = ImageFile
     extra = 1
